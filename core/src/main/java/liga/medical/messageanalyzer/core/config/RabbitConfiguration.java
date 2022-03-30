5f0c0580a37b1660b341e7e0d8bbd2c8276cdcb1
Dimka Maximov <dimka_maximov@mail.ru>
2022-03-30 19:32:40 +0300

    Commit with changes from Main project

--- core/src/main/java/liga/medical/messageanalyzer/core/config/RabbitConfiguration.java
+++ core/src/main/java/liga/medical/messageanalyzer/core/config/RabbitConfiguration.java
@@ -1,38 +1,23 @@
 package liga.medical.messageanalyzer.core.config;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-import org.springframework.messaging.handler.annotation.support.MessageHandlerMethodFactory;
-
 
 @Configuration
-public class RabbitConfiguration implements RabbitListenerConfigurer {
-
-    @Override
-    public void configureRabbitListeners(RabbitListenerEndpointRegistrar registrar) {
-        registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
-    }
-
-    @Bean
-    MessageHandlerMethodFactory messageHandlerMethodFactory() {
-        DefaultMessageHandlerMethodFactory messageHandlerMethodFactory = new DefaultMessageHandlerMethodFactory();
-        messageHandlerMethodFactory.setMessageConverter(new MappingJackson2MessageConverter());
-        return messageHandlerMethodFactory;
-    }
+@Slf4j
+public class RabbitConfiguration {
 
     @Bean
     public ConnectionFactory connectionFactory() {
+
         return new CachingConnectionFactory("localhost");
     }
 
@@ -42,15 +27,13 @@ public class RabbitConfiguration implements RabbitListenerConfigurer {
     }
 
     @Bean
-    public RabbitTemplate rabbitTemplate(){
-        //return new RabbitTemplate(connectionFactory());
-        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory());
-        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
-        return rabbitTemplate;
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
     }
 
     @Bean
-    public Queue medicalQueue(){
+    public Queue myQueue1() {
         return new Queue("medical");
     }
+
 }
